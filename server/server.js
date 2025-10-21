b51a6119c8a4fc9332f602de7f79606eaa06cb05
Daniel Afriyie <dafriyie7@gmail.com>
2025-10-21 14:04:50 +0000

    installed updated cors

--- server/server.js
+++ server/server.js
@@ -12,7 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("combined"))
-app.use(cors());
+app.use(cors({
+	origin: "*"
+}));
 
 app.get("/", (req, res) => res.send("server running"));
 
